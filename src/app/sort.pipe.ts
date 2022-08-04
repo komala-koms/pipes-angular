132d2b47c75a9bca2d4840a2ea8eb1aff487472f
komala-koms <komaladunnadk@gmail.com>
2022-08-04 21:01:39 +0530

    first commit

---
+++ src/app/sort.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'sort',
+  pure : false
+})
+export class SortPipe implements PipeTransform {
+
+  transform(value: any,propertyName:string) {
+    return value.sort((a,b)=>
+      {
+        if(a[propertyName]>b[propertyName])
+        {
+          return 1;
+        }
+        else
+          return -1;
+      }
+    );
+  }
+
+}
